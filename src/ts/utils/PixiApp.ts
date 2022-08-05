c9c2432effb5e910ff2e892aebd9c21a9aec3683
Xelun <eliromeva@gmail.com>
2022-08-05 15:18:22 +0200

    fix: Fix load pixi assets in parallel

--- src/ts/utils/PixiApp.ts
+++ src/ts/utils/PixiApp.ts
@@ -11,12 +11,14 @@ export interface PixiAppConfig extends PIXI.IApplicationOptions {
 export class PixiApp {
     protected _app: PIXI.Application;
     protected _onResizeList: ((width: number, height: number) => any)[];
+    protected _assetQueue: any[];
 
     constructor(config: PixiAppConfig = {}) {
         if(!config.view) config.view = document.getElementById(config.canvasName ?? "canvas") as HTMLCanvasElement;
 
         this._app = new PIXI.Application(config);
         this._onResizeList = [];
+        this._assetQueue = [];
 
         // Recommended concurrency to download assets is minimum 100 (pixi have it as 10 by default)
         PIXI.Loader.shared.concurrency = 100;
@@ -49,14 +51,27 @@ export class PixiApp {
     }
 
     /** Load an asset on Pixi */
-    public loadAsset(name, path): Promise<any> {
+    public loadAsset(name: string, url: string): Promise<void> {
         return new Promise((resolve) => {
-            PIXI.Loader.shared.add({
-                name: name,
-                url: path,
-                onComplete: (resource: PIXI.LoaderResource) => resolve(resource),
-            });
-            PIXI.Loader.shared.load();
+            if(PIXI.Loader.shared.loading) {
+                this._assetQueue.push({ assets: [ { name, url } ], resolve });
+            } else {
+                PIXI.Loader.shared.add({ name, url });
+                PIXI.Loader.shared.load(() => this._loadedFinish(resolve));
+            }
+        });
+    }
+
+    public loadAssets(assets: { name: string, url: string }[]): Promise<void> {
+        return new Promise((resolve) => {
+            if(PIXI.Loader.shared.loading) {
+                this._assetQueue.push({ assets, resolve });
+            } else {
+                assets.forEach(asset => {
+                    PIXI.Loader.shared.add({ name: asset.name, url: asset.url });
+                });
+                PIXI.Loader.shared.load(() => this._loadedFinish(resolve));
+            }
         });
     }
 
@@ -120,5 +135,29 @@ export class PixiApp {
     /** Add a new action when the app change size */
     public onResize(fn: (width: number, height: number)=> any) {
         this._onResizeList.push(fn);
+    }
+
+    /** Check iof there is still pending assets to be loaded as once the loader is started, no new assets can be added to be load */
+    protected _loadedFinish(resolve) {
+        if(this._assetQueue.length > 0) {
+            PIXI.Loader.shared.reset();
+
+            let resolves = [];
+            this._assetQueue.forEach(assetInfo => {
+                assetInfo.assets.forEach(asset => {
+                    PIXI.Loader.shared.add({
+                        name: asset.name,
+                        url: asset.url,
+                    });
+                });
+                resolves.push(assetInfo.resolve);
+            })
+
+            this._assetQueue = [];
+            PIXI.Loader.shared.load(() => this._loadedFinish(resolves));
+        }
+
+        if(typeof resolve === "function") resolve();
+        else resolve.forEach(res => res());
     }
 }
